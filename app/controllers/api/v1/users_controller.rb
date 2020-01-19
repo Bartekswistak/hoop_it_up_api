8f9d0025d605f9db959e1b3dc242dd66ee29593f
BartekSwistak <bartekswistak24@gmail.com>
2020-01-19 10:48:10 -0800

    cleaned up controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,38 +1,37 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
     def index
-        @users = User.all
-    
-        render json: @users
-        # users_json = UserSerializer.new(@users).serialized_json
-        # render json: users_json
-      end
+      @users = User.all
+      render json: @users
+    end
 
-      def show
-        render json: @user
-        # user_json = UserSerializer.new(@user).serialized_json
-        # render json: user_json
-      end
+    def new
+      @user = User.new
+    end
+
+    def show
+      @user = set_user
+      render json: @user
+    end
     
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id
-          render json: @user
-        #   render json: UserSerializer.new(@user), status: :created
-        else
-          render json: @user.errors, status: :unprocessable_entity
-        end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
       end
+    end
 
       private
 
-      def set_user
-          @user = User.find(params[:id])
-        end
+    def set_user
+      @user = User.find(params[:id])
+    end
   
-      def user_params
-          params.require(:user).permit(:username, :email, :password)
-      end
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
